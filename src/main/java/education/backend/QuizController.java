25ab67cd03bfe9bdaf32e0a8683d38ad1322d73a
robokae <alexanderhom19@gmail.com>
2022-03-05 20:52:16 -0800

    update QuizController

--- src/main/java/education/backend/QuizController.java
+++ src/main/java/education/backend/QuizController.java
@@ -1,13 +1,24 @@
 package education.backend;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequestMapping("/quiz")
 public class QuizController {
     
-    @GetMapping("/quiz")
+    @GetMapping
     public String quiz() {
         return "quiz";
     }
+
+    @PostMapping
+    public String handleQuizSubmit() {
+        // grade the quiz the user submitted
+
+        return "redirect:/quizResults";
+    }
+
 }
