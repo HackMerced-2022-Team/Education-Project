6f6f2bc7d075c435c9e14b814e5c929d983ec313
DaoistDuck <dragonoath123@gmail.com>
2022-03-05 17:43:08 -0800

    Create HomeController.java

---
+++ src/main/java/education/backend/HomeController.java
@@ -0,0 +1,20 @@
+// package education.backend;
+
+// import org.springframework.stereotype.Controller;
+// import org.springframework.ui.Model;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+
+// // @ prefix => Java annotation
+// @Controller
+// public class HomeController {
+
+// // route
+// @GetMapping("/")
+// public String home(@RequestParam(name = "name", required = false,
+// defaultValue = "World") String name,
+// Model model) {
+// model.addAttribute("name", name);
+// return "home";
+// }
+// }
